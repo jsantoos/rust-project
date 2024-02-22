f3adf0e39816698403715c1b34f1d4c80b6c5d98
João Santos <joaosantos@greenole.com>
2024-02-22 14:47:39 -0300

    rust project

---
+++ section01/main.rs
@@ -0,0 +1,16 @@
+/* Usando rustc			[1.2. Hello, World!]
+
+Baseado em:
+The Rust Programming Language
+by Steve Klabnik and Carol Nichols, with contributions from the Rust Community
+This version of the text assumes you’re using Rust 1.67.1 (released 2023-02-09) or later
+https://doc.rust-lang.org/stable/book/
+
+*/
+		
+		
+fn main() {
+	println!("Hello, world.");
+}
+
+
